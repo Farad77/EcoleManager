0f31fd2caf0427f4970df15c7e4c1a6a558b7ec5
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2021-12-01 13:22:28 +0400

    Modif du controlleur Etudiant

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -13,7 +13,7 @@ class EtudiantController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('etudiant/index.html.twig', [
+        return $this->render('bidule/index.html.twig', [
             'controller_name' => 'EtudiantController',
         ]);
     }
